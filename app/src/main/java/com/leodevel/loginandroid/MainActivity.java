ebdd2edd4f8860fc6bfad4115b0865e329e2446c
Leonardo Barbosa <leobar1995@gmail.com>
2016-10-07 21:43:47 -0300

    SDK do facebook adicionado. Adicionado String application_key do facebook. Meta-data do facebook adicionado dentro de application do arquivo AndroidManifest.xml. Chamada de função do facebook analytics dentro do método OnCreate da Activity de Login

--- app/src/main/java/com/leodevel/loginandroid/MainActivity.java
+++ app/src/main/java/com/leodevel/loginandroid/MainActivity.java
@@ -3,11 +3,16 @@ package com.leodevel.loginandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
     }
 }
